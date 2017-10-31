b2acbda210f03d76a1f6c01f9eadc78e5a381e3c
azimme2s <artur.zimmermann@outlook.de>
2017-10-31 22:49:06 +0100

    Updated the service worker for offline work

--- se1-pwa/service-worker.js
+++ se1-pwa/service-worker.js
@@ -5,12 +5,23 @@
 
     var filesToCache = [
         '/',
-        'ccm.se1-menu.js',
-        'ccm.nav-hamburger.js',
-        'resources/style.css',
-        'ccm.min.js',
+        '/ccm.se1-menu.js',
+        '/ccm.nav-hamburger.js',
+        '/resources/style.css',
         'http://kaul.inf.h-brs.de/data/2017/se1/le00.html',
         'http://kaul.inf.h-brs.de/data/2017/se1/le01.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le02.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le03.html',
+       /* 'http://kaul.inf.h-brs.de/data/2017/se1/le04.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le05.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le06.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le07.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le08.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le09.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le10.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le11.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le12.html',
+        'http://kaul.inf.h-brs.de/data/2017/se1/le13.html',*/
         'http://kaul.inf.h-brs.de/data/ccm/form/ccm.form.js',
         'http://kaul.inf.h-brs.de/data/ccm/upload/ccm.upload.js',
         'http://kaul.inf.h-brs.de/data/ccm/highlight/ccm.highlight.js',
@@ -19,37 +30,58 @@
         'http://kaul.inf.h-brs.de/data/ccm/form/resources/default.css',
         'https://akless.github.io/ccm-components/user/versions/ccm.user-2.0.0.min.js',
         'https://akless.github.io/ccm-components/log/versions/ccm.log-1.0.0.min.js',
+        'https://akless.github.io/ccm-components/content/versions/ccm.content-2.0.0.min.js',
+        'https://akless.github.io/ccm/ccm.js',
         'http://kaul.inf.h-brs.de/data/2017/se1/json/log_configs.js',
         'https://akless.github.io/ccm-components/libs/md5/md5.min.js'
     ];
-    self.addEventListener('fetch', function(event) {
-        event.respondWith(
+    self.addEventListener('fetch', function (event) {
+        /*event.respondWith(
             caches.open(cacheName).then(function(cache) {
                 return cache.match(event.request).then(function(response) {
                     var fetchPromise = fetch(event.request).then(function(networkResponse) {
                         cache.put(event.request, networkResponse.clone());
                         return networkResponse;
-                    })
+                    });
                     return response || fetchPromise;
                 })
             })
+        );*/
+        /*console.log("The service worker is serving the asset.");
+        event.respondWith(fromNetwork(event.request, 400).catch(function () {
+            return fromCache(event.request);
+        }));*/
+        event.respondWith(
+            caches.match(event.request)
+                .then(function (response) {
+                        if (response) {
+                            console.log(
+                                '[fetch] Returning from ServiceWorker cache: ',
+                                event.request.url
+                            );
+                            return fromCache(event.request);
+                        }
+                        console.log('[fetch] Returning from server: ', event.request.url);
+                        return fromNetwork(event.request, 200);
+                    }
+                )
         );
     });
 
-    self.addEventListener('install', function(e) {
+    self.addEventListener('install', function (e) {
         console.log('[ServiceWorker] Install');
         e.waitUntil(
-            caches.open(cacheName).then(function(cache) {
+            caches.open(cacheName).then(function (cache) {
                 console.log('[ServiceWorker] Caching app shell');
                 return cache.addAll(filesToCache);
             })
         );
     });
-    self.addEventListener('activate', function(e) {
+    self.addEventListener('activate', function (e) {
         console.log('[ServiceWorker] Activate');
         e.waitUntil(
-            caches.keys().then(function(keyList) {
-                return Promise.all(keyList.map(function(key) {
+            caches.keys().then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
                     if (key !== cacheName && key !== dataCacheName) {
                         console.log('[ServiceWorker] Removing old cache', key);
                         return caches.delete(key);
@@ -59,4 +91,22 @@
         );
         return self.clients.claim();
     });
+
+    function fromNetwork(request, timeout) {
+        return new Promise(function (fulfill, reject) {
+            var timeoutId = setTimeout(reject, timeout);
+            fetch(request).then(function (response) {
+                clearTimeout(timeoutId);
+                fulfill(response);
+            }, reject);
+        });
+    }
+
+    function fromCache(request) {
+        return caches.open(cacheName).then(function (cache) {
+            return cache.match(request).then(function (matching) {
+                return matching || Promise.reject('no-match');
+            });
+        });
+    }
 }());
