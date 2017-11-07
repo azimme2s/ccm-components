ca19e5fa4f36670765faa186cb18e68faa26b05e
azimme2s <artur.zimmermann@outlook.de>
2017-11-07 23:39:38 +0100

    Adding a PWA delete ccm-button

---
+++ delete-pwa-button/ccm.delete-pwa-button.js
@@ -0,0 +1,50 @@
+(function (){
+    var component = {
+        name: "delete-pwa-button",
+        ccm: "https://akless.github.io/ccm/ccm.js",
+        config: {
+            html:{
+                footer:{
+                    tag: 'footer',
+                    class: 'footer',
+                    inner:{
+                        tag: 'button',
+                        class: 'button',
+                        id: 'delete-pwa-button',
+                        inner: 'Delete PWA'
+                    }
+                }
+            }
+        },
+        Instance: function(){
+            let self = this;
+            let my;
+
+            this.ready = function( callback ) {
+                my = self.ccm.helper.privatize( self );
+                if( callback ) callback();
+            };
+            this.start = function (callback) {
+                this.buildView();
+                self.element.querySelector('#delete-pwa-button').addEventListener('click', self.deletePWA);
+                if(callback) callback();
+            };
+            this.buildView = function(){
+                let footer = self.ccm.helper.html(my.html.footer);
+                self.element.appendChild(footer);
+            };
+            this.deletePWA = function(){
+                navigator.serviceWorker.getRegistrations().then(function(registrations) {
+                    console.log("I have this: ", registrations);
+                    for(let registration of registrations) {
+                        registration.unregister()
+                    } })
+            }
+        }
+    };
+    function p(){
+        window.ccm[v].component(component);
+    }
+    var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
+
+})();
