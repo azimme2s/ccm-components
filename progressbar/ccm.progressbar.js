88c432c54fb4550e89db9a9a44a398b6cdde01e7
azimme2s <artur.zimmermann@outlook.de>
2018-10-12 19:53:56 +0200

    made some new changes

--- progressbar/ccm.progressbar.js
+++ progressbar/ccm.progressbar.js
@@ -1,85 +1,72 @@
-(function () {
-    let component = {
-        name: "progressbar",
-        ccm: "https://akless.github.io/ccm/ccm.js",
+/**
+ * @overview ccm component for progress bar
+ * @author Arthur Zimmermann 2018
+ * @license MIT License
+ * @version 1.0.0
+ * @changes
+ * version 1.0.0 (12.10.2018)
+ */
+
+( function () {
+
+    const component = {
+
+        name: 'progressbar',
+
+        ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+
         config: {
-            html: {
-                progressbar: {
-                    tag: 'div',
-                    class: 'progress-bar',
-                    inner:{
-                        tag: 'div',
-                        class: 'progress-bar-inner',
-                        inner: ''
+            "html": {
+                "progressbar": {
+                    "tag": "div",
+                    "class": "progress-bar",
+                    "inner": {
+                        "tag": "div",
+                        "class": "progress-bar-inner",
+                        "inner": ""
                     }
                 }
             },
-            css: ['ccm.load', 'https://ccmjs.github.io/azimmer-components/progressbar/resources/default.css'],
+            "css": [ "ccm.load", "https://ccmjs.github.io/azimmer-components/progressbar/resources/default.css" ],
         },
 
         Instance: function () {
-            let self = this;
-            let my;
-
-            this.ready = async () => {
-                my = self.ccm.helper.privatize(self);
-            };
 
             this.start = async () => {
-                let main_elem = self.ccm.helper.html(my.html.progressbar);
-                self.element.appendChild(main_elem);
 
-                this.setComplete(my.min);
+                this.ccm.helper.setContent( this.element, this.ccm.helper.html( this.html.progressbar ) );
+
+                this.setComplete( this.min );
                 let _complete = this.getComplete();
 
+                for ( let i = this.min; i <= this.max; i++ )
+                    setTimeout( () => { _complete = i; this.setComplete( _complete ); }, 100 * i );
 
-                for(let i= my.min; i <= my.max; i++) {
-                    setTimeout(function () {
-                        _complete = i;
-                        self.setComplete(_complete);
-                    }, 100 * i);
-                }
             };
-            this.setComplete = function(value){
-                let newValue = value / my.max;
+
+            this.setComplete = value => {
+
+                let newValue = value / this.max;
                 newValue *= 100;
-                let innerBar = self.element.querySelector('.progress-bar-inner');
-                if(innerBar.style.width !== "100%") {
+                let innerBar = this.element.querySelector( '.progress-bar-inner' );
+                if ( innerBar.style.width !== '100%' ) {
                     innerBar.style.width = newValue + '%';
-                    if(my.showText){
-                        innerBar.innerHTML = value + my.sign;
-                    }
+                    if ( this.showText )
+                        innerBar.innerHTML = value + this.sign;
                 }
-                if (newValue > 100) {
+                if ( newValue > 100 ) {
                     innerBar.style.width = '100%';
-                    if(my.showText){
-                        innerBar.innerHTML = value + my.sign;
-                    }
+                    if ( this.showText )
+                        innerBar.innerHTML = value + this.sign;
                 }
+
             };
-            this.getComplete = function() {
-                return my.complete;
-            };
+
+            this.getComplete = () => this.complete;
 
         }
+
     };
 
-
-    function p() {
-        window.ccm[v].component(component);
-    }
-
-    var f = "ccm." + component.name + (component.version ? "-" + component.version.join(".") : "") + ".js";
-    if (window.ccm && null === window.ccm.files[f]) window.ccm.files[f] = component; else {
-        var n = window.ccm && window.ccm.components[component.name];
-        n && n.ccm && (component.ccm = n.ccm), "string" == typeof component.ccm && (component.ccm = {url: component.ccm});
-        var v = component.ccm.url.split("/").pop().split("-");
-        if (v.length > 1 ? (v = v[1].split("."), v.pop(), "min" === v[v.length - 1] && v.pop(), v = v.join(".")) : v = "latest", window.ccm && window.ccm[v]) p(); else {
-            var e = document.createElement("script");
-            document.head.appendChild(e), component.ccm.integrity && e.setAttribute("integrity", component.ccm.integrity), component.ccm.crossorigin && e.setAttribute("crossorigin", component.ccm.crossorigin), e.onload = function () {
-                p(), document.head.removeChild(e)
-            }, e.src = component.ccm.url
-        }
-    }
-
-})();
+    let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||["latest"])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){window.ccm[c].component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+} )();
