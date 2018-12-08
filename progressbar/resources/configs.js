9360bbc9692b2a31f80799e2f33b830ffe3cbea5
azimme2s <artur.zimmermann@outlook.de>
2018-12-08 12:01:03 +0100

    Updated some files

---
+++ progressbar/resources/configs.js
@@ -0,0 +1,30 @@
+/**
+ * @overview confirgurtion of the progress bar
+ * @author Artur Zimmermann <artur.zimmermann@outlook.de> 2018
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ 'configs.js' ] = {
+
+    "local": {
+
+        "key": "local",
+        "css.1": "resources/default.css",
+        "complete": "0",
+        "sign": '%',
+        "showText": "true",
+        "min": "0",
+        "max": "100"
+
+    },
+
+    "demo": {
+
+        "key": "demo",
+        "complete": "0",
+        "sign": '%',
+        "showText": "true",
+        "min": "0",
+        "max": "100"
+    }
+};
